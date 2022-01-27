0854c06d86d3dd0da7c61443eb5429e41e5d7b98
Hyeon Ju <89682253+kimhyeonju@users.noreply.github.com>
2022-01-27 15:48:00 +0900

    Delete script2.js

--- script2.js
+++
@@ -1,9 +0,0 @@
-var modal = document.getElementById("modal");
-var button1 = document.getElementById("enroll");
-var button2 = document.getElementById("close");
-button1.onclick = function(){
-    modal.style.display = "block";
-}
-button2.onclick = function(){
-    modal.style.display = "none";
-}
